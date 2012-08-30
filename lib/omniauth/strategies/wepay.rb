47b5c8dfbc67180c7a6a45a0fba1f53cff1aa000
lvxn0va <lvx.n0va@gmail.com>
2012-08-30 11:04:12 -0700

    switch to oa-oauth wepay code - testing

--- lib/omniauth/strategies/wepay.rb
+++ lib/omniauth/strategies/wepay.rb
@@ -1,52 +1,70 @@
-require 'omniauth-oauth2'
+require 'omniauth/oauth'
+require 'multi_json'
 
 module OmniAuth
   module Strategies
-    class Wepay < OmniAuth::Strategies::OAuth2
-
-      DEFAULT_PERMISSIONS = "manage_accounts,view_balance,collect_payments,refund_payments,view_user,preapprove_payments"
-
-      option :fields, [:user_id, :name, :email]
-      option :uid_field, :user_id
-      option :scope, DEFAULT_PERMISSIONS
-      option :client_options, {
-        :authorize_url  => "https://stage.wepay.com/v2/oauth2/authorize",
-        :token_url      => "https://stage.wepayapi.com/v2/oauth2/token",
-        :site           => "https://stage.wepayapi.com/v2",
-        :user_path      => "/user"
-      }
-
-      uid do
-        raw_info["user_id"]
+    # OAuth 2.0 based authentication with WePay. In order to
+    # sign up for an application, you need to [register an application](https://wepay.com/developer/register)
+    # and provide the proper credentials to this middleware.
+    #
+    # Authenticate to WePay utilizing OAuth 2.0 and retrieve
+    # basic user information.
+    #
+    # @example Basic Usage
+    # use OmniAuth::Strategies::WePay, 'client ID', 'client secret'
+    class WePay < OmniAuth::Strategies::OAuth2
+      # @param [Rack Application] app standard middleware application argument
+      # @param [String] client_id the application ID for your client
+      # @param [String] client_secret the application secret
+      # @option options [Hash]
+      # :scope - the application permissions you are requesting. Defaults to all.
+      # [manage_accounts,view_balance,collect_payments,refund_payments,view_user]
+      def initialize(app, client_id=nil, client_secret=nil, options={}, &block)
+        client_options = {
+          :authorize_url  => "https://stage.wepay.com/v2/oauth2/authorize",
+          :token_url      => "https://stage.wepayapi.com/v2/oauth2/token",
+          :site           => "https://stage.wepayapi.com/v2",
+          :user_path      => "/user"
+        }
+        super(app, :wepay, client_id, client_secret, client_options, options, &block)
       end
 
-      info do 
-        {"email"=> raw_info["email"], "name"=> "#{raw_info['first_name']} #{raw_info['last_name']}"}
+      protected
+
+      def user_data
+        @data ||= MultiJson.decode(@access_token.get('https://stage.wepay.com/v2/user').body)
       end
 
-      def raw_info
-        c_options = options.client_options
+      def request_phase
+        @options[:scope] ||= "manage_accounts,view_balance,collect_payments,refund_payments,view_user,preapprove_payments"
+        super
+      end
+      
+      def callback_phase
+        # Clear out options. WePay token call will fail if unknown keys are passed to it.
+        @options = {}
+        super
+      end
 
-        params = {
-          code: request.params['code'], 
-          client_id:options.client_id, 
-          redirect_uri: callback_url, 
-          client_secret: options.client_secret
-        }.to_param
+      def user_info
+        {
+          'email' => user_data['email'],
+          'name' => "#{user_data['first_name']} #{user_data['last_name']}",
+          'first_name' => user_data['first_name'],
+          'last_name' => user_data['last_name']
+        }
+      end
 
-        url = c_options[:token_url] + "?" + params
-
-        info = access_token.post(url).parsed
-
-        user_url = c_options[:site] + c_options[:user_path]
-        
-        info2 = client.request(:post, user_url) do |req|
-          req.headers["Authorization"] = "Bearer #{info["access_token"]}"
-        end.parsed
-        
-        @raw_info ||= info.merge(info2)
+      def auth_hash
+        OmniAuth::Utils.deep_merge(super, {
+          'uid' => @access_token.params['user_id'],
+          'user_info' => user_info,
+          'extra' => {
+            'user_hash' => user_data,
+            'token_type' => @access_token.params['token_type']
+          }
+        })
       end
     end
   end
 end
-
---
+++ lib/omniauth/strategies/wepay_.rb
@@ -0,0 +1,52 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Wepay < OmniAuth::Strategies::OAuth2
+
+      DEFAULT_PERMISSIONS = "manage_accounts,view_balance,collect_payments,refund_payments,view_user,preapprove_payments"
+
+      option :fields, [:user_id, :name, :email]
+      option :uid_field, :user_id
+      option :scope, DEFAULT_PERMISSIONS
+      option :client_options, {
+        :authorize_url  => "https://stage.wepay.com/v2/oauth2/authorize",
+        :token_url      => "https://stage.wepayapi.com/v2/oauth2/token",
+        :site           => "https://stage.wepayapi.com/v2",
+        :user_path      => "/user"
+      }
+
+      uid do
+        raw_info["user_id"]
+      end
+
+      info do 
+        {"email"=> raw_info["email"], "name"=> "#{raw_info['first_name']} #{raw_info['last_name']}"}
+      end
+
+      def raw_info
+        c_options = options.client_options
+
+        params = {
+          code: request.params['code'], 
+          client_id:options.client_id, 
+          redirect_uri: callback_url, 
+          client_secret: options.client_secret
+        }.to_param
+
+        url = c_options[:token_url] + "?" + params
+
+        info = access_token.post(url).parsed
+
+        user_url = c_options[:site] + c_options[:user_path]
+        
+        info2 = client.request(:post, user_url) do |req|
+          req.headers["Authorization"] = "Bearer #{info["access_token"]}"
+        end.parsed
+        
+        @raw_info ||= info.merge(info2)
+      end
+    end
+  end
+end
+
